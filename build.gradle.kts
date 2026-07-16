4dd295a85f4980d0b025ce79ee957c322d30b586
Patryk Miś <foss@patrykmis.com>
2026-07-16 04:09:03 +0200

    Move Kotlin Gradle Plugin classpath to version catalog
    Signed-off-by: Patryk Miś <foss@patrykmis.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,9 @@
 plugins {
     alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
+}
+
+buildscript {
+    dependencies {
+        classpath(libs.kotlin.gradle.plugin)
+    }
 }
